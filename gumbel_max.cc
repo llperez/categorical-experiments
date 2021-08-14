9fca1399b52f591a6d3aae959fb280fc5c362e8d
lp6 <lperezp@gmail.com>
2021-08-14 15:22:08 -0600

    gumbel_max doc

--- gumbel_max.cc
+++ gumbel_max.cc
@@ -9,7 +9,10 @@ unsigned int gumbel_max(const double *p, unsigned int length, const gsl_rng *r) 
   double max_val = log(p[0]) - log(-log(gsl_rng_uniform(r)));
   for (unsigned int i=1;i<length;i++) {
 
+    // gumbel draw
     double c_val = log(p[i]) - log(-log(gsl_rng_uniform(r)));
+
+    // update max
     if (c_val > max_val) {
       which_max = i;
       max_val = c_val;
@@ -25,7 +28,10 @@ unsigned int gumbel_max_log_scale(const double *p, unsigned int length, const gs
   double max_val = p[0] - log(-log(gsl_rng_uniform(r)));
   for (unsigned int i=1;i<length;i++) {
 
+    // gumbel draw
     double c_val = p[i]  - log(-log(gsl_rng_uniform(r)));
+
+    // update max
     if (c_val > max_val) {
       which_max = i;
       max_val = c_val;
@@ -37,10 +43,13 @@ unsigned int gumbel_max_log_scale(const double *p, unsigned int length, const gs
 
 gumbel_max_precomp_t *gumbel_max_precomp_create(size_t size, gsl_rng *r) {
 
+  // allocate structure
   gumbel_max_precomp_t *out = (gumbel_max_precomp_t *)malloc(sizeof(gumbel_max_precomp_t));
   out->samples = (double *)malloc(sizeof(double) * size);
   out->size = size;
 
+
+  // add the gumbel draws
   for (size_t i=0;i<size;i++) {
     out->samples[i] = log(-log(gsl_rng_uniform(r)));
   }
@@ -55,16 +64,20 @@ void gumbel_max_precomp_free(gumbel_max_precomp_t *gmp) {
 
 unsigned int gumbel_max_precomp(const double *p, unsigned int length, const gsl_rng *r, const gumbel_max_precomp_t *gmp) {
 
+
+  // pick the starting position in the Gumbel-max structure at random
   int s_pos = gsl_rng_uniform_int(r, gmp->size);
   int which_max = 0;
   double max_val = log(p[0]) - gmp->samples[s_pos++];
 
   for (unsigned int i=1;i<length;i++) {
 
+    // get next random Gumbel draw and move forward
     double gum_val = gmp->samples[(s_pos % gmp->size)];
     double c_val = log(p[i]) - gum_val;
     s_pos++;
 
+    // update max
     if (c_val > max_val) {
       which_max = i;
       max_val = c_val;
@@ -77,16 +90,20 @@ unsigned int gumbel_max_precomp(const double *p, unsigned int length, const gsl_
 
 unsigned int gumbel_max_precomp_log_scale(const double *p, unsigned int length, const gsl_rng *r, const gumbel_max_precomp_t *gmp) {
 
+  // pick the starting position in the Gumbel-max structure at random
   int s_pos = gsl_rng_uniform_int(r, gmp->size);
   int which_max = 0;
   double max_val = p[0] - gmp->samples[s_pos++];
 
   for (unsigned int i=1;i<length;i++) {
 
+    // get the next random Gumbel draw and move forward
     double gum_val = gmp->samples[(s_pos % gmp->size)];
     double c_val = p[i] - gum_val;
     s_pos++;
 
+
+    // update max
     if (c_val > max_val) {
       which_max = i;
       max_val = c_val;
--- gumbel_max.h
+++ gumbel_max.h
@@ -3,18 +3,45 @@
 
 #include <gsl/gsl_rng.h>
 
+
+/**
+ * Structure for keeping precomputed for Gumbel samples.
+ */
 typedef struct gumbel_max_precomp_t {
   double *samples;
   size_t size;
 } gumbel_max_precomp_t;
 
+
+/**
+ * Returns a sample using the Gumbel-max method.
+ */
 unsigned int gumbel_max(const double *p, unsigned int length, const gsl_rng *r);
+
+/**
+ * Returns a sample using the Gumbel-max method (with probabilities in log-scale).
+ */
 unsigned int gumbel_max_log_scale(const double *p, unsigned int length, const gsl_rng *r);
 
+/**
+ * Generates a precomputed Gumbel sample structure.
+ */
 gumbel_max_precomp_t *gumbel_max_precomp_create(size_t size, gsl_rng *r);
+
+/**
+ * Frees the precomputed Gumbel samples structure.
+ */
 void gumbel_max_precomp_free(gumbel_max_precomp_t *gmp);
 
+/**
+ * Returns a sample using the Gumbel-max method, with precomputed samples.
+ */
 unsigned int gumbel_max_precomp(const double *p, unsigned int length, const gsl_rng *r, const gumbel_max_precomp_t *gmp);
+
+/**
+ * Returns a sample using the Gumbel-max method, with precomputed
+ * samples and probabilities in the log-scale.
+ */
 unsigned int gumbel_max_precomp_log_scale(const double *p, unsigned int length, const gsl_rng *r, const gumbel_max_precomp_t *gmp);
 
 #endif // _GUMBEL_MAX_H
